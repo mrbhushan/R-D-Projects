4ed3ef96a64a304177e9b33f9a217473590485bc
Bhushan Saler <bhushan.saler@hotmail.com>
2018-05-04 15:20:55 +0530

    Add project files.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,35 @@
+﻿namespace Calculator
+{
+   public interface ICalculator
+    {
+        int Add(int a, int b);
+        int Subtract(int a, int b);
+        int Divide(int a, int b);
+
+        int Multiply(int a, int b);
+    }
+
+    public class Calculator : ICalculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
+}
---
+++ UnitTestCalculator/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿
+using Calculator;
+using NUnit.Framework;
+
+namespace UnitTestCalculator
+{
+    [TestFixture]
+    public class CalculatorTestClass
+    {
+        Calculator.Calculator cal = new Calculator.Calculator();
+
+        [TestCase(1,1,2)]
+        [TestCase(-1, 1, 0)]
+        [TestCase(1000, 122, 1122)]
+        public void CanAddnumber(int x,int y,int expected)
+        {
+            Assert.That(cal.Add(x, y), Is.EqualTo(expected));
+
+        }
+
+
+
+        public void CanDivideNumber(int x,int y, int expected)
+    }
+}
